62ad25973eece3cecc4c8a5705564a674aa5ca2a
Azib <Azib@IMRANSAEED>
2023-02-20 19:06:39 +0500

    initial Push

---
+++ ComputerServicesWeb/ComputerServicesWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComputerServicesWeb.Startup))]
+namespace ComputerServicesWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
