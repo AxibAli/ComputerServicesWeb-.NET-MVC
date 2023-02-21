414f81fc2c284a350b3e4b5d8965610c8100679b
bilal khalid <bilal.khalid@braincorp.net>
2023-02-21 19:47:05 +0500

    Login and Role Work

---
+++ ComputerServicesWeb/ComputerServicesWeb/DataBase/ComputerServiceWebContext.cs
@@ -0,0 +1,17 @@
+﻿using ComputerServicesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ComputerServicesWeb.DataBase
+{
+    public class ComputerServiceWebContext:DbContext
+    {
+        public ComputerServiceWebContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
